fb7e4a9f275bc392166ccda0ea558144e8b91f25
DiffuseHyperion <xieyingjie08@gmail.com>
2022-05-08 10:21:58 +0800

    add bukkit and bungee portion

---
+++ bukkit/update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+cd /data
+apt-get update -y
+apt-get install openjdk-8-jre-headless -y
+apt-get install curl -y
+apt-get install unzip -y
+rm craftbukkit-1.5.2-R1.0.jar
+
+# i know i can download the jar directly, but it causes the jar to be corrupt, idk why
+mkdir /data/tmp
+cd /data/tmp
+curl -L -O https://github.com/LAX1DUDE/eaglercraft/raw/main/stable-download/stable-download-new.zip
+unzip stable-download-new.zip
+rm -r web
+rm -r java/bungee_command
+mv java/bukkit_command/craftbukkit-1.5.2-R1.0.jar /data/craftbukkit-1.5.2-R1.0.jar
+cd /data
+rm -r tmp
+
+echo "File update done!"
+
+if [ ! -f /data/eula.txt ]
+then
+    echo "eula=true" > /data/eula.txt
+    echo "EULA has been set to true! Please read https://account.mojang.com/documents/minecraft_eula."
+fi
+
+echo Running server with $1-$2 RAM!
+java -Xms$1 -Xmx$2 -jar craftbukkit-1.5.2-R1.0.jar
---
+++ bungee/update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd /data
+apt-get update -y
+apt-get install openjdk-8-jre-headless -y
+apt-get install curl -y
+apt-get install unzip -y
+rm bungee-dist.jar
+
+# i know i can download the jar directly, but it causes the jar to be corrupt, idk why
+mkdir /data/tmp
+cd /data/tmp
+curl -L -O https://github.com/LAX1DUDE/eaglercraft/raw/main/stable-download/stable-download-new.zip
+unzip stable-download-new.zip
+rm -r web
+rm -r java/bukkit_command
+mv java/bungee_command/bungee-dist.jar /data/bungee-dist.jar
+cd /data
+rm -r tmp
+
+echo "File update done!"
+
+echo Running server with $1-$2 RAM!
+# flags for optimizing server: https://aikar.co/mcflags.html
+java -Xms$1 -Xmx$2 -jar bungee-dist.jar
